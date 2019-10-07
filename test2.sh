83e23607c2bc129c1bce3f5c517c55cf8c22f6e3
MeichengWang <56236589+MeichengWang@users.noreply.github.com>
2019-10-07 11:14:00 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,13 +1,16 @@
+read-p'Start Time':start
+read-p'End Time':end
+
 bash /mapreduce-test/start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab2/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../BigDataAsn/mapper1.py -mapper ../BigDataAsn/mapper1.py \
--file ../BigDataAsn/reducer2.py -reducer ../BigDataAsn/reducer2.py \
+-file ../Lab1Mapreduce/mapper2.py -mapper ../Lab1Mapreduce/mapper2.py \
+-file ../Lab1Mapreduce/reducer2.py -reducer ../BigDataAsn/reducer2.py \
 -input /lab1/input/* -output /lab1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /lab1/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/output/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/output/
 bash /mapreduce-test/stop.sh
